7d4cce9196338df7cbc5ec1efe9260d7bab3d7b6
Jdquimbiulco <jdquimbiulco@espe.edu.ec>
2025-05-12 22:30:56 -0500

    uploading files

---
+++ Pry_Espe/public/cargar.js
@@ -0,0 +1,11 @@
+function cargarPaginas(nombre_pagina) {
+    fetch(nombre_pagina)
+        .then(res => res.text())
+        .then(data => document.getElementById("contenido").innerHTML = data)
+        .catch(err => console.error("Error al cargar la página:", err));
+}
+
+// Configuración inicial para cargar una página por defecto
+window.onload = () => {
+    cargarPaginas("/paginas/carusel.html"); // Página inicial por defecto
+};
